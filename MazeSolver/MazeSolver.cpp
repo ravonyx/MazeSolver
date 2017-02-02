c407017c70d93ba718562e0a5a8de169c7fca92b
DESKTOP-JVPO5O1\Ravonyx <ravonyxa@gmail.com>
2017-02-02 23:47:32 +0100

    list folders and choose img

--- MazeSolver/MazeSolver.cpp
+++ MazeSolver/MazeSolver.cpp
@@ -6,10 +6,83 @@
 #include <opencv2/video/tracking.hpp>
 #include <iostream>
 
+#include <Windows.h>
+#include <tchar.h> 
+#include <stdio.h>
+#include <strsafe.h>
+
+void listFolders(std::vector<std::string> &imgNames)
+{
+	WIN32_FIND_DATA file;
+	HANDLE hFind = NULL;
+	LARGE_INTEGER filesize;
+	TCHAR* filePath = L"img";
+
+	TCHAR path[2048];
+	DWORD dwError = 0;
+	StringCchCopy(path, MAX_PATH, filePath);
+	StringCchCat(path, MAX_PATH, TEXT("\\*"));
+
+	if ((hFind = FindFirstFile(path, &file)) == INVALID_HANDLE_VALUE)
+	{
+		printf("Path not found: [%s]\n", filePath);
+		return;
+	}
+	do
+	{
+		if (file.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+		{
+		}
+		else
+		{
+			filesize.LowPart = file.nFileSizeLow;
+			filesize.HighPart = file.nFileSizeHigh;
+
+			std::wstring ws(file.cFileName);
+			std::string str(ws.begin(), ws.end());
+			imgNames.push_back(str);
+		}
+	} while (FindNextFile(hFind, &file) != 0);
+
+	dwError = GetLastError();
+	if (dwError != ERROR_NO_MORE_FILES)
+		printf("No more files");
+
+	FindClose(hFind);
+}
+
+std::string loadMazeImage(std::vector<std::string> &imgNames)
+{
+	for (int i = 0; i < imgNames.size(); i++)
+	{
+		std::cout << "(" << i << ") " << imgNames[i] << std::endl;
+	}
+	std::cout << "Enter nb:";
+	int choose;
+	std::cin >> choose;
+
+	if (choose > imgNames.size() || choose < 0)
+	{
+		std::cout << "Wrong number given !" << std::endl;
+		exit(0);
+	}
+
+	std::string name = imgNames[choose];
+	std::string pathImg = "Img/";
+	pathImg += name;
+	std::cout << pathImg << std::endl;
+
+	return pathImg;
+}
+
 int main()
 {
+	std::vector<std::string> imgNames;
+	listFolders(imgNames);
+	std::string pathImg = loadMazeImage(imgNames);
+
 	cv::Mat imgMaze;
-	imgMaze = cv::imread("img/maze1.png");
+	imgMaze = cv::imread(pathImg);
 	cv::Mat copyImgMaze(imgMaze.size(), CV_8UC3, cv::Scalar(255, 255, 255));
 	imgMaze.copyTo(copyImgMaze);
 	cv::imshow("Original image", copyImgMaze);
@@ -22,7 +95,6 @@ int main()
 	double maxValue = 255;
 
 	cv::threshold(img, img, 50, maxValue, cv::THRESH_BINARY_INV);
-
 	cv::Mat kernel = cv::Mat::ones(2, 2, CV_8UC1);
 	std::vector<std::vector<cv::Point> > contours;
 	cv::findContours(img, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
@@ -47,21 +119,15 @@ int main()
 	cv::absdiff(dilated, eroded, diff);
 	diff.convertTo(diff, CV_8UC1);
 
-	cv::Mat bgr(imgMaze.size(), CV_8UC3, cv::Scalar(255,255,255));
 	//set pixels masked by blackWhite to blue
-	bgr.setTo(cv::Scalar(255, 0, 0), diff);
-	bgr.convertTo(bgr, CV_8UC1);
-	cv::imshow("Path", bgr);
+	cv::Mat path(imgMaze.size(), CV_8UC3, cv::Scalar(255,255,255));
+	path.setTo(cv::Scalar(255, 0, 0), diff);
+	path.convertTo(path, CV_8UC1);
+	cv::imshow("Path", path);
 
-	//cv::Mat result(imgMaze.size(), CV_8UC3, cv::Scalar(255, 255, 255));
-	//cv::add(copyImgMaze, diff, result);
-	std::vector<cv::Mat> array_to_merge;
-	array_to_merge.push_back(copyImgMaze);
-	array_to_merge.push_back(bgr);
-
-	cv::Mat color(imgMaze.size(), CV_8UC3);
-	cv::merge(array_to_merge, color);
-	//cv::imshow("Merged image", array_to_merge);
+	cv::Mat finalImg;
+	copyImgMaze.copyTo(finalImg, path);
+	imshow("Maze Resolved", finalImg);
 
 	cv::waitKey();
 	return 1;
